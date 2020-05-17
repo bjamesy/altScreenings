5ad45e906e8911435596809a21b8a8961bf00fd2
james ballanger <jamesballanger@trentu.ca>
2020-05-17 19:22:37 -0400

    pooling connection string added to transaction query

--- db/seedQueries.js
+++ db/seedQueries.js
@@ -1,10 +1,12 @@
-const db       = require('./index');
-const { Pool } = require('pg')
-    , pool     = new Pool();
+const db               = require('./index');
+const { Pool }         = require('pg')
+const connectionString = 'postgres://fsoehenxfuofyg:0838ffd43dcceb07a8f4fb524c3f685c7bb812e4b691861a24ef909f09be4c23@ec2-52-71-85-210.compute-1.amazonaws.com:5432/d1sjibbrj48n8f';
+const pool             = new Pool({
+    connectionString: connectionString
+});
 
 module.exports =  {
     async seedScreening(screenings, theatre, url) {
-        console.log('trying to connect here');
         const client = await pool.connect();
 
         try {
