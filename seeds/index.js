433c534edd916f3f7e5e6cf9ca6194a3c1b5a116
james ballanger <jamesballanger@trentu.ca>
2020-08-01 16:04:50 -0400

    promise.all() put in place to make asyncronous seed calls and underscraping check

--- seeds/index.js
+++ seeds/index.js
@@ -1,5 +1,6 @@
 const { deleteSeeds } = require('../db/seedQueries');
-const { db }          = require('../db/seedQueries');
+const db              = require('../db/index');
+const sgMail          = require('@sendgrid/mail');
 const { 
     getRoyal, 
     getParadise, 
@@ -11,34 +12,43 @@ const {
 } = require('../seeds/seed');
 
   // SeedDB scraping all sites 
-async function seedDB() {
-    // remove seeding
-    deleteSeeds();
-    // begin seeding
-    getCinesphere(0);
-    getRegent(0);
-    getTiff(0);
-    getRoyal(0);
-    getParadise(0);
-    getRevue(0);
-    getHotDocs(0);  
-    
-    // CHECK for underscraping - and likely error
+function seedDB() {
+    let seedPromises = [
+        // remove seeding
+        deleteSeeds(),
+        // begin seeding
+        getCinesphere(0),
+        getRegent(0),
+        getTiff(0),
+        getRoyal(0),
+        getParadise(0),
+        getRevue(0),
+        getHotDocs(0) 
+    ];
+
+    // When seeding Promises and db work done - then check 
+    Promise.all(seedPromises).then(() => { 
+        underScrapingCheck();
+    })    
+};
+
+// CHECK for underscraping - and likely error
+async function underScrapingCheck() {
     let theatreSql = 'SELECT * FROM theatre;'
     const theatreCheck = await db.query(theatreSql);
     // if detected by a less than 7 theatre check - notify me
-    if (theatreCheck.rows.length !== 7 ) {
+    if(theatreCheck.rows.length !== 7 ) {
         console.log('TWILIO controller detects that there are less than 7 theatres in the database BOY! : ');
         // send email
         const msg = {
-            to: "jamesballanger@trentu.ca",
+            to: process.env.personalEmail,
             from: `IST Admin <${process.env.myEmail}>`,
             subject: 'SCRAPING ERROR !',
             html: 'get ur ass onto heroku u bum - no ones getting updates dont worry'
         }
-        await sgMail.send(msg);            
+        await sgMail.send(msg);      
         return;
-    }
-};
+    }    
+}
 
 module.exports = { seedDB }; 
