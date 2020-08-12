ad7c394e6be92cec382cbbc7bd20bc290340bdd0
james ballanger <jamesballanger@trentu.ca>
2020-08-12 11:59:16 -0400

    MORE testing

--- middleware/index.js
+++ middleware/index.js
@@ -24,6 +24,10 @@ module.exports = {
         if(error.includes('Cannot read property') && error.includes('querySelectorAll') && error.includes('null') && i < 4) {
             console.log(`RESEED ${theatre} querySelectorALL error ${i}: `, error);
             i++;
+            if(i = 3) {
+                console.log(rerun);
+                console.log(i)    
+            }
             rerun(i);
         }                            
     }
